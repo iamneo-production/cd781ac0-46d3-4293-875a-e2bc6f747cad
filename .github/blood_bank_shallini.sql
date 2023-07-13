a4a405a6879c0450e9af0bff762e1b58a8f1401a
shalu-2039 <shalinipugal2002@gmail.com>
2023-07-13 14:00:14 +0000

    commited

--- .github/blood_bank_shallini.sql
+++ .github/blood_bank_shallini.sql
@@ -1,21 +1,21 @@
--- question 1
+-- num 1
 select state,count(*) as blood_bank_count
 from BLOOD_BANK
 group by state;
--- question 2
+-- num 2
 create index bloodbank name on blood_bank(state);
 select "Blood Bank Name"
 from blood_bank
 where state='Andhra Pradesh';
--- question 3
+-- num 3
 select count(*) as tblood_bank_count
 from BLOOD_BANK;
--- question 4
+-- num 4
 select "Blood Bank Name"
 from BLOOD_BANK
 where rownum<=5
 order by "Blood Bank Name" asc;
--- question 5
+-- num 5
 select "Blood Bank Name"
 from BLOOD_BANK
 order by "Blood Bank Name" asc;

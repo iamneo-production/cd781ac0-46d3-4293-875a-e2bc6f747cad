2123725ebba402f9df6fc321caa7d64ec247764d
rajolishreya <rajolishreya@gmail.com>
2023-07-13 09:12:28 +0000

    Query committed

---
+++ .github/workflows/blood_bank_RR_Shreya.sql
@@ -0,0 +1,24 @@
+/* 1st question */
+select state,count(*) as totcount_state
+from BLOOD_BANK
+group by state;
+
+/* 2nd question */
+select "Blood Bank Name"
+from BLOOD_BANK
+where state = 'Andhra Pradesh';
+
+/* 3rd question */
+select count(*) as totcount_bloodbank
+from BLOOD_BANK;
+
+/* 4th question */
+select "Blood Bank Name"
+from BLOOD_BANK
+where ROWNUM<=5
+order by "Blood Bank Name" ASC;
+
+/* 5th question */
+select "Blood Bank Name"
+from BLOOD_BANK
+

9e9cfae0632dc1fbb69e5454e8c291bad4768546
shalu-2039 <shalinipugal2002@gmail.com>
2023-07-13 06:50:24 +0000

    i have commit the query

--- .settings/blood_bank_shallini.sql
+++ .settings/blood_bank_shallini.sql
@@ -1,19 +1,20 @@
+-- no 1
 select state,count(*) as blood_bank_count
 from BLOOD_BANK
 group by state;
-
+-- no 2
 select "Blood Bank Name"
 from blood_bank
 where state='Andhra Pradesh';
-
+-- no 3
 select count(*) as tblood_bank_count
 from BLOOD_BANK;
-
+-- no 4
 select "Blood Bank Name"
 from BLOOD_BANK
 where rownum<=5
 order by "Blood Bank Name" asc;
-
+-- no 5
 select "Blood Bank Name"
 from BLOOD_BANK
 order by "Blood Bank Name" asc;

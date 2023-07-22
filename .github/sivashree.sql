659ab82d3f4f4c5626adb4a0796caa56450f44cf
Sivashreek <sivashreekumar3110@gmail.com>
2023-07-22 07:06:21 +0000

    commit

---
+++ .github/sivashree.sql
@@ -0,0 +1,19 @@
+select state,count(*) as blood_bank_count
+from BLOOD_BANK
+group by state;
+-- num2
+select "Blood Bank Name"
+from blood_bank
+where state='Andhra Pradesh';
+-- num 3
+select count(*) as tblood_bank_count
+from BLOOD_BANK;
+-- num 4
+select "Blood Bank Name"
+from BLOOD_BANK
+where rownum<=5
+order by "Blood Bank Name" asc;
+-- num 5
+select "Blood Bank Name"
+from BLOOD_BANK
+order by "Blood Bank Name" asc;

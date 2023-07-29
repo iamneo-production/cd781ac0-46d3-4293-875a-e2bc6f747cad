141cd94a2e8c75f0b9b57337335c3fa8f3cdd8c6
GaneshDSG <ganeshdsg150@gmail.com>
2023-07-29 10:28:11 +0000

    Ganesh

--- Ganesh_DSG.sql
+++ Ganesh_DSG.sql
@@ -1,5 +1,5 @@
-/* query -1 no.of blood banks in state */
-/*set timer on*/
+--query -1 no.of blood banks in state 
+--set timer on
 SELECT
     STATE,
     count(STATE) NO_OF_BLOOD_BANKS
@@ -8,7 +8,7 @@ FROM
 GROUP BY
     state
 ORDER BY state;
-/* Query-2 blood banks in Andhra pradesh */
+--Query-2 blood banks in Andhra pradesh
 select
     "Blood Bank Name"
 from
@@ -17,11 +17,11 @@ where
     state like 'Andh%';
 --Query-3 total blood bank
 select 
-    count(id) as "total blood banks" 
+    count(id) AS "total blood banks" 
 from 
     BLOOD_BANK;
 
-/* query 4 first 5 blood banks */
+--query 4 first 5 blood banks
 SELECT
     "Blood Bank Name"
 FROM
@@ -31,7 +31,7 @@ ORDER BY
 OFFSET 0 ROWS
 FETCH FIRST 5 ROWS ONLY;
 
-/* QUERY 5 Blood bank  names in alphabetical order */
+--QUERY 5 Blood bank  names in alphabetical order
 SELECT
     "Blood Bank Name"
 FROM

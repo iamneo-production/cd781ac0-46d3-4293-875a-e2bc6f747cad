924a5a4ff3cf3455216616c540cea829dc0b76d8
sivapetta <sivasatyanarayana987@gmail.com>
2023-07-30 18:52:59 +0000

    changed

---
+++ SivaSatyanarayana_Petta.sql
@@ -0,0 +1,24 @@
+-- question 1
+select state,count(*) as tcount_state
+from blood_bank
+group by state;
+
+-- question 2
+select "Blood Bank Name"
+from blood_bank
+where state='Andhra Pradesh';
+
+-- questions 3
+select count(*) as tcount_bloodbank
+from blood_bank;
+
+-- question 4
+select "Blood Bank Name"
+from BLOOD_BANK
+order by "Blood Bank Name" asc
+where rownum<=5;
+
+-- question 5
+select "Blood Bank Name"
+from blood_bank
+order by "Blood Bank Name" asc;

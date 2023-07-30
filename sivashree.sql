a1bb1e7283d1c6c1b841a7663975f704ac477b10
Sivashreek <sivashreekumar3110@gmail.com>
2023-07-30 17:41:50 +0000

    changes

--- sivashree.sql
+++ sivashree.sql
@@ -1,31 +1,24 @@
-<<<<<<< HEAD:sivashree.sql
+-- query one
 select state,count(*) as blood_bank_count
 from BLOOD_BANK
 group by state;
--- num2
-=======
--- number 1
-select state,count(*) as blood_bank_count
-from BLOOD_BANK
-group by state;
---  number 2
->>>>>>> 18869749933cf4161f64b7c22621b82e72927d37:shalini_pugalenthi.sql
+
+-- query two
 select "Blood Bank Name"
 from blood_bank
 where state='Andhra Pradesh';
--- number 3
+
+-- query three
 select count(*) as tblood_bank_count
 from BLOOD_BANK;
--- number 4
+
+-- query four 
 select "Blood Bank Name"
 from BLOOD_BANK
 order by "Blood Bank Name" asc
 where rownum<=5;
-<<<<<<< HEAD:sivashree.sql
--- num 5
-=======
--- number 5
->>>>>>> 18869749933cf4161f64b7c22621b82e72927d37:shalini_pugalenthi.sql
+
+-- query five
 select "Blood Bank Name"
 from BLOOD_BANK
 order by "Blood Bank Name" asc;

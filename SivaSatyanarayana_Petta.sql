9537f62be31b1a4aa362cc4e054314a52c21488f
sivapetta <sivasatyanarayana987@gmail.com>
2023-07-30 19:48:20 +0000

    1 18

--- SivaSatyanarayana_Petta.sql
+++ SivaSatyanarayana_Petta.sql
@@ -3,16 +3,12 @@ select state,count(*) as tcount_state
 from blood_bank
 group by state;
 
-<<<<<<< HEAD:siri_chandana_sukkala.sql
---question_2 
-=======
 -- question 2
->>>>>>> 924a5a4ff3cf3455216616c540cea829dc0b76d8:SivaSatyanarayana_Petta.sql
 select "Blood Bank Name"
 from blood_bank
 where state='Andhra Pradesh';
 
--- question 3
+-- questions 3
 select count(*) as tcount_bloodbank
 from blood_bank;
 
@@ -22,7 +18,7 @@ from BLOOD_BANK
 order by "Blood Bank Name" asc
 where rownum<=5;
 
--- question 5 
+-- question 5
 select "Blood Bank Name"
 from blood_bank
 order by "Blood Bank Name" asc;

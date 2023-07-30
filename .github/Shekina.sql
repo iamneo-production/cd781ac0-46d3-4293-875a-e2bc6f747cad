1371393c82b9d078b0e7b6ba34c623a9f5f18956
Shekina577 <dandalashekina@gmail.com>
2023-07-30 20:04:54 +0000

    commit

--- .github/Shekina.sql
+++ .github/Shekina.sql
@@ -1,19 +1,20 @@
-select state,count(*) as blood_bank_count
+-- Query1
+select state, count(*) as bank_count
 from BLOOD_BANK
 group by state;
-
-select *
+-- Query2
+select "Blood Bank Name"
 from BLOOD_BANK
-where state = 'Andhra Pradesh';
-
-select COUNT(*) as BLOOD_BANK
+where state='Andhra Pradesh';
+-- Query3
+select count(*) as count_bloodbank
 from BLOOD_BANK;
-
+-- Query4
 select "Blood Bank Name"
 from BLOOD_BANK
-where rownum<=5
-order by "Blood Bank Name" ASC;
-
+order by "Blood Bank Name" asc
+where rownum<=5;
+-- Query5
 select "Blood Bank Name"
 from BLOOD_BANK
-order by "Blood Bank Name";
+order by "Blood Bank Name" asc;

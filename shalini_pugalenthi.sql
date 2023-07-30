029fc6281437ec7f4a4c27f912c2e01821a24e15
shalu-2039 <shalinipugal2002@gmail.com>
2023-07-30 19:28:26 +0000

    commit

--- shalini_pugalenthi.sql
+++ shalini_pugalenthi.sql
@@ -1,20 +1,20 @@
--- query 1
+-- Query 1
 select state,count(*) as blood_bank_count
 from BLOOD_BANK
 group by state;
---  query 2
+--  Query 2
 select "Blood Bank Name"
 from blood_bank
 where state='Andhra Pradesh';
--- query 3
+-- Query 3
 select count(*) as tblood_bank_count
 from BLOOD_BANK;
--- query 4
+-- Query 4
 select "Blood Bank Name"
 from BLOOD_BANK
 order by "Blood Bank Name" asc
 where rownum<=5;
--- query 5
+-- Query 5
 select "Blood Bank Name"
 from BLOOD_BANK
 order by "Blood Bank Name" asc;

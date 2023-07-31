b3e6120945d4f81902a30633da33b55fc7c15b73
shalu-2039 <shalinipugal2002@gmail.com>
2023-07-31 05:33:16 +0000

    commit

--- shalini_pugalenthi.sql
+++ shalini_pugalenthi.sql
@@ -1,20 +1,31 @@
--- number 1
-select state,count(*) as blood_bank_count
-from BLOOD_BANK
+-- question 1
+select state,count(*) as tcount_state
+from blood_bank
 group by state;
---  number 2
+
+-- question 2
 select "Blood Bank Name"
 from blood_bank
 where state='Andhra Pradesh';
--- number 3
-select count(*) as tblood_bank_count
-from BLOOD_BANK;
--- number 4
+
+-- questions 3
+select count(*) as tcount_bloodbank
+from blood_bank;
+
+-- question 4
+SELECT
+    "Blood Bank Name"
+FROM
+    BLOOD_BANK
+ORDER BY 
+    "Blood Bank Name" ASC
+OFFSET 0 ROWS
+FETCH FIRST 5 ROWS ONLY;
+
+-- question 5
 select "Blood Bank Name"
-from BLOOD_BANK
-order by "Blood Bank Name" asc
-where rownum<=5;
--- number 5
-select "Blood Bank Name"
-from BLOOD_BANK
+from blood_bank
 order by "Blood Bank Name" asc;
+
+
+

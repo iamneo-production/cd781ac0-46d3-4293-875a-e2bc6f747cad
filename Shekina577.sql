78544413f4a9cbd3c18780fb58170a35f9376c43
Shekina577 <dandalashekina@gmail.com>
2023-07-31 07:44:58 +0000

    13 14

--- Shekina577.sql
+++ Shekina577.sql
@@ -2,13 +2,16 @@
 select state,count(*) as blood_bank_count
 from BLOOD_BANK
 group by state;
+
 -- Query 2
 select "Blood Bank Name"
 from blood_bank
 where state='Andhra Pradesh';
+
 -- Query 3
 select COUNT(*) as BLOOD_BANK
 from BLOOD_BANK;
+
 -- Query 4
 select
     "Blood Bank Name"
@@ -18,6 +21,7 @@ order by
     "Blood Bank Name" asc
 offset 0 rows
 fetch first 5 rows only;
+
 -- Query 5
 select "Blood Bank Name"
 from BLOOD_BANK

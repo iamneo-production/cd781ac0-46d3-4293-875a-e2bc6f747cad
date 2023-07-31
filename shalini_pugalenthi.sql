41eafcf2ee8fa2e869d218c86b88588be4afed80
shalu-2039 <shalinipugal2002@gmail.com>
2023-07-31 05:20:48 +0000

    commit

--- shalini_pugalenthi.sql
+++ shalini_pugalenthi.sql
@@ -13,11 +13,16 @@ select count(*) as tcount_bloodbank
 from blood_bank;
 
 -- question 4
-select "Blood Bank Name"
-from BLOOD_BANK
-where rownum<=5
-order by "Blood Bank Name" asc;
+SELECT
+    "Blood Bank Name"
+FROM
+    BLOOD_BANK
+ORDER BY 
+    "Blood Bank Name" ASC
+OFFSET 0 ROWS
+FETCH FIRST 5 ROWS ONLY;
 -- question 5
 select "Blood Bank Name"
 from blood_bank
 order by "Blood Bank Name" asc;
+
